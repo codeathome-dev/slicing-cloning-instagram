0d45ab3fb700afea37a284c5142d2e1b7648578c
elfinsanjaya12 <elfinsanjaya1207@gmail.com>
2021-10-21 18:59:56 +0700

    slicing instagram

---
+++ assets/js/showbar.js
@@ -0,0 +1,10 @@
+
+function menuLike() {
+    var element = document.getElementById("menu-like");
+    element.classList.toggle("showMenuLike");
+}
+
+function menuProfile() {
+    var element = document.getElementById("menu-profile");
+    element.classList.toggle("showmenuProfile");
+}
